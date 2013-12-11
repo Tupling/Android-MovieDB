e420190eefbc544513e25ed9c7a794be1acf3589
Dale Tupling <tupling32@hotmail.com>
2013-12-10 21:04:21 -0600

    Added MovieDetailsActivty class
    Added MovieDetails class as well as renamed folder structure. This
    project will remain in one folder instead of redoing the project each
    week.

---
+++ Java2_Android_App/MovieDB/src/com/daletupling/movies/MovieDetailsActivity.java
@@ -0,0 +1,19 @@
+/*
+ * Project:        MoviesDB
+ * 
+ * Package: com.daletupling.movies
+ * 
+ * Author:         Dale Tupling
+ * 
+ * Date:        December 10th, 2013
+ * 
+ */
+
+
+package com.daletupling.movies;
+
+import android.content.Intent;
+
+public class MovieDetailsActivity extends Intent {
+
+}
